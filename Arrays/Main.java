2ababec62b1cf64919cb55b2327f8afbd2c9ba1a
Vivek Kumar <vivek.k@zillionica.com>
2024-12-21 11:56:15 +0530

    Print all Subarray element

---
+++ Arrays/Main.java
@@ -0,0 +1,10 @@
+package DSA.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = { 1, 2, 3};
+        PrintSubArrayElement printSubArrayElement = new PrintSubArrayElement();
+        printSubArrayElement.printSubArrayElement(array);
+    }
+
+}
